3a758ec782a0d60a52f6aed7738b605dac7dfcb0
AlbiDota <alisaele1@gmail.com>
2025-03-08 20:16:53 +0100

    favicon + test workflow

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,14 +8,8 @@ const Home = () => {
     return (
         <div className="App">
             <img src={logo} className="App-logo" alt="logo" /> <br/><br/><br/><br/><br/><br/><br/><br/>
-            <h1>LOGG INN</h1>
-            <LoggInn/> <br/><br/><br/><br/><br/><br/><br/><br/>
-
-            <h1>KONTAKT OSS</h1>
-            <KontaktOss/> <br/><br/><br/><br/><br/><br/><br/><br/>
-
-            <h1>WORKOUT FORM</h1>
-            <WorkoutForm/> <br/><br/><br/><br/><br/><br/><br/><br/>
+            <h1>react-bert</h1>
+            
 
         </div>
     );
